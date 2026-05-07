c441cc4e1fe1276fa54503c915beb62e065079a9
Johan <johan.mantilla01@epn.edu.ec>
2026-05-06 22:19:40 -0500

    refactor: remove unused methods and imports from vehicle restriction service

--- src/vehicle-restriction/vehicle-restriction.controller.ts
+++ src/vehicle-restriction/vehicle-restriction.controller.ts
@@ -1,7 +1,6 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { VehicleRestrictionService } from './vehicle-restriction.service';
 import { CreateVehicleRestrictionDto } from './dto/create-vehicle-restriction.dto';
-import { UpdateVehicleRestrictionDto } from './dto/update-vehicle-restriction.dto';
 
 @Controller('vehicle-restriction')
 export class VehicleRestrictionController {
--- src/vehicle-restriction/vehicle-restriction.service.ts
+++ src/vehicle-restriction/vehicle-restriction.service.ts
@@ -1,8 +1,6 @@
 import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateVehicleRestrictionDto } from './dto/create-vehicle-restriction.dto';
-import { UpdateVehicleRestrictionDto } from './dto/update-vehicle-restriction.dto';
 import { VehicleRestrictionRecord } from './interfaces/vehicle-restriction-record.interface.ts';
-import { DAY_NAMES, RESTRICTED_DIGITS_BY_DAY } from './constants/pico-y-placa.constants';
 import { PicoYPlacaRule } from './domain/pico-y-placa.rule';
 
 @Injectable()
@@ -41,50 +39,5 @@ export class VehicleRestrictionService {
     }
     return record;
   }
-
-  private canCirculateVehicle(createVehicleRestrictionDto) {
-
-    const lastDigit = createVehicleRestrictionDto.licensePlate.slice(-1);
-    const lastDigitAsNumber = this.convertStringToInteger(lastDigit);
-
-    const date = this.convertStringToDate(createVehicleRestrictionDto.date);
-    const currentDay = DAY_NAMES[date.getDay()];
-
-    const restrictedDigits = RESTRICTED_DIGITS_BY_DAY[currentDay];
-
-    const isRestricted = restrictedDigits.includes(lastDigitAsNumber);
-    console.log(isRestricted);
-
-    const time = this.canCirculateVehicleByHour(createVehicleRestrictionDto.time);
-    console.log(time);
-
-    if (isRestricted && time) {
-      return false;
-    }
-
-    return true;
-  }
-
-  private convertStringToInteger(lastDigit) {
-    return parseInt(lastDigit);
-  }
-
-  private convertStringToDate(date): Date {
-    return new Date(date + 'T00:00:00');
-  }
-
-  private canCirculateVehicleByHour(time): boolean {
-
-    const timeList = time.split(":");
-
-    const hour = this.convertStringToInteger(timeList[0]);
-    const minute = this.convertStringToInteger(timeList[1]);
-
-    const timeToMinutes = (hour * 60) + minute;
-    console.log(timeToMinutes);
-
-    return (timeToMinutes >= 420 && timeToMinutes <= 570) || (timeToMinutes >= 960 && timeToMinutes <= 1170);
-  }
-
 
 }
